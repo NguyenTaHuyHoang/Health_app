257e62a97b55f29532689bab981fcdad9fea2dca
Hải Nguyễn <20120468@student.hcmus.edu.vn>
2022-11-30 10:45:54 +0700

    update interface admin and employee

--- src/app/controllers/EmployeeController.js
+++ src/app/controllers/EmployeeController.js
@@ -1,13 +1,29 @@
 const Employee = require('../models/employee');
 const mongooseHelper = require('../util/mongoose');
-
+const Invoice = require('../models/invoice');
+const employeeHelper = require('../util/employeeHelper');
 class EmployeeController {
     // get interface
     // [GET] /employee
     interface(req, res, next) {
-        res.render("employee", {
-            title: `Employee: ${req.params.id}`
-        });
+        let obj;
+
+        Invoice.find({}).then(
+            invoice => {
+                obj = mongooseHelper.multiMongooseToObject(invoice);
+            }
+        ).catch(next);
+
+        Employee.findOne({ _id: req.params.id }).then(employee => {
+            res.render("employee", {
+                title: `Employee: ${req.params.id}`,
+                employee: mongooseHelper.mongoosesToObject(employee),
+                ListInvoice: employeeHelper.getListInvoice(obj),
+                ListPatient: employeeHelper.getListPatient(),
+                ListAppointment: employeeHelper.getListAppointment(),
+            });
+        })
+            .catch(next);
     }
 
     // [POST] /employee/login
