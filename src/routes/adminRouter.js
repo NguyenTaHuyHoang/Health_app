3956173eda65c0a0bcde3182a49c65cbd325a43a
20120468 <20120468@student.hcmus.edu.vn>
2022-12-09 00:12:33 +0700

    update func add employee

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const adminRouter = require("../app/controllers/AdminController");
 
+router.get('/getAPI/employee', adminRouter.getAPIEmployee);
 router.get('/getAPI/client', adminRouter.getAPIClient);
+router.post('/add/employee', adminRouter.addEmployee);
 router.post('/add/Client', adminRouter.addClient);
 router.post("/login", adminRouter.checkLogin);
 router.get("/:id", adminRouter.interface);
