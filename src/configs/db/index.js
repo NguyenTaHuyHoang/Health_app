29cdca8725cb933601b38334c8cdf19f39872de6
20120468 <20120468@student.hcmus.edu.vn>
2022-12-31 16:29:19 +0700

    update footer and cloud db

--- src/configs/db/index.js
+++ src/configs/db/index.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
 
 async function connect() {
+
   try {
-    await mongoose.connect("mongodb://localhost:27017/Hospital", {
+    let connectionString = "mongodb+srv://hospital:hospital123@cluster0.ybwogqe.mongodb.net/Hospital?retryWrites=true&w=majority";
+    mongoose.connect(connectionString, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      //useCreateIndex: true,mongodb://localhost:27017/
+      useUnifiedTopology: true
     });
-    console.log("Successfully connected!");
-  } catch (error) {
-    console.log(error);
+
+    //mongodb://localhost:27017/Hospital
+    //mongodb+srv://hospital:hospital123@cluster0.ybwogqe.mongodb.net/Hospital?retryWrites=true&w=majority
+    const connection = mongoose.connection;
+    connection.on('error', console.error.bind(console, 'connection error:'));
+    connection.once('open', function () {
+      // we're connected!
+      console.log('Connected to MongoDB Atlas');
+    });
   }
+  catch (e) {
+    console.log(e);
+  }
+
 }
 
 module.exports = { connect };
